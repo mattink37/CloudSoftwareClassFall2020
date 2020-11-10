b6f73788f7060e621132d224f81358968de1424f
mattinkeles <mattinkeles37@gmail.com>
2020-11-10 18:52:57 -0500

    Moved projects and added homework4

---
+++ mattinkeleswebapp/WebApp/WebApp/Pages/GradeEntry.cshtml.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace WebApp.Pages
+{
+    public class GradeEntryModel : PageModel
+    {
+        SqlConnection connection;
+        public List<Student> students { get; set; }
+        public GradeEntryModel()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "cloudcomputingclassdb.database.windows.net";
+            builder.UserID = "mattink37";
+            builder.Password = "a:U7wp_a";
+            builder.InitialCatalog = "CloudComputingClassDB";
+            connection = new SqlConnection(builder.ConnectionString);
+        }
+
+        public void OnPost()
+        {
+            var id = Request.Form["id"];
+            var grade = Request.Form["grade"];
+
+            string query = $"insert into Grades (id, grade) values ('{id}', '{grade}');";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            connection.Close();
+        }
+
+        public void OnGet()
+        {
+            string query = "select * from grades";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            students = new List<Student>();
+            while (reader.Read())
+            {
+                var studentId = reader.GetValue(0);
+                var grade = reader.GetValue(1);
+
+                var studentExists = false;
+                foreach (var student in students)
+                {
+                    if (student.id == (int)studentId)
+                    {
+                        student.grades.Add(Convert.ToSingle(grade));
+                        studentExists = true;
+                        break;
+                    }
+                }
+                if (!studentExists)
+                {
+                    students.Add(new Student((int)studentId, new List<float>() { Convert.ToSingle(grade) }));
+                }
+            }
+            connection.Close();
+        }
+    }
+}
